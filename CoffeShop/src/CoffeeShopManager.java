d2740d257cf4a6a75a77f6e218a66b3e99056fad
Jorge Escobar <95109695+jorgefsa23@users.noreply.github.com>
2023-06-21 19:59:36 -0300

    Add files via upload

--- CoffeShop/src/CoffeeShopManager.java
+++ CoffeShop/src/CoffeeShopManager.java
@@ -21,37 +21,43 @@ public class CoffeeShopManager {
                 "\n4. Empty/restart bag\n5. Pay\n6. Exit";
         JOptionPane.showMessageDialog(null, message_welcome);
         while (true) {
-            int option = Integer.parseInt(showInputDialog(null, message_options, "CoffeShop Manager", JOptionPane.QUESTION_MESSAGE));
 
-            switch (option) {
-                case 1:
-                    placeOrder();
-                    break;
-                case 2:
-                    summarizeOrder();
-                    break;
-                case 3:
-                    cancelItem();
-                    break;
-                case 4:
-                    cancelProcess();
-                    break;
-                case 5:
-                    pay();
-                    break;
-                case 6:
-                    JOptionPane.showMessageDialog(null, "Thank you for visiting our coffee shop!");
-                    return;
-                default:
-                    JOptionPane.showMessageDialog(null, "Invalid option, please try again");
+
+            try{
+                int option = Integer.parseInt(showInputDialog(null, message_options, "CoffeShop Manager", JOptionPane.QUESTION_MESSAGE));
+                switch (option) {
+                    case 1:
+                        placeOrder();
+                        break;
+                    case 2:
+                        summarizeOrder();
+                        break;
+                    case 3:
+                        cancelItem();
+                        break;
+                    case 4:
+                        cancelProcess();
+                        break;
+                    case 5:
+                        pay();
+                        break;
+                    case 6:
+                        JOptionPane.showMessageDialog(null, "Thank you for visiting our coffee shop!");
+                        return;
+                    default:
+                        JOptionPane.showMessageDialog(null, "This option doesn't exists,\nplease try again");
+                }
+            } catch (NumberFormatException e){
+                JOptionPane.showMessageDialog(null, "Please enter a valid Integer");
             }
+
         }
     }
 
     private void placeOrder() {
         flavors.listFlavors();
-        int flavorOption = Integer.parseInt(showInputDialog(null, "What flavor would you like to add?"));
-        int quantity = Integer.parseInt(showInputDialog(null, "How many bags do you want?"));
+        int flavorOption = Integer.parseInt(showInputDialog(null, "What flavor (number) would you like to add?"));
+        int quantity = Integer.parseInt(showInputDialog(null, "How many cups do you want?"));
         bag.addItem(flavors.getFlavor(flavorOption), quantity, flavors.getPrice(flavorOption));
         JOptionPane.showMessageDialog(null, "Item added into the bag", "Done!", JOptionPane.INFORMATION_MESSAGE);
     }
@@ -85,8 +91,9 @@ public class CoffeeShopManager {
             } else {
                 JOptionPane.showMessageDialog(null, "Yor payment by: " + paymentOption + " was rejected", "Warning!", JOptionPane.WARNING_MESSAGE);
             }
+
         } else {
-            JOptionPane.showMessageDialog(null, "No items in bag", "NO Payment to process!", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Bag is empty!", "NO Payment to process!", JOptionPane.INFORMATION_MESSAGE);
         }
 
     }
